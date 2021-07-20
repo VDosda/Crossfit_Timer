d9ed4f5b7a103fcef48d8a3cc822d473a852b2ef
Victor Dosda <victordosda@gmail.com>
2021-07-20 13:19:37 +0200

    fix display of a '0' because of the second interval not setup

--- Code/src/Timer.ino
+++ Code/src/Timer.ino
@@ -39,7 +39,7 @@ void TimerPreparationCountDown(){
           cleanDisplay(4);
           hide_fourth_display_flag = true;
        }
-       if (SEC_TIMER < 3 && SEC_TIMER > 0){
+       if (SEC_TIMER < 4 && SEC_TIMER > 0){
           if (sec_tampon_buzzer != SEC_TIMER){
             sec_tampon_buzzer = SEC_TIMER;
             buzzerOn(500);
@@ -106,6 +106,8 @@ void timeMapMS(int Min, int Sec, int diffSec){
 }
 
 
+bool H1_null_flag;
+bool H2_null_flag;
 void intervalTimerUP(){
    if(pre_start_flag){
         pre_start_flag=false;
@@ -115,9 +117,22 @@ void intervalTimerUP(){
     if(!timer_preparation_10_sec_flag && running_flag){
        if(setup_timer_flag){
               setup_timer_flag=false;
-              timeMapMS(MIN_TIMER_TARGET_H1, SEC_TIMER_TARGET_H1, -1);
+              
+              H1_null_flag = (MIN_TIMER_TARGET_H1 + SEC_TIMER_TARGET_H1) == 0;
+              H2_null_flag = (MIN_TIMER_TARGET_H2 + SEC_TIMER_TARGET_H2) == 0;
+               
+              if(H1_null_flag && !H2_null_flag){                          //if there is only one interval set otherwize set H1 
+                timeMapMS(MIN_TIMER_TARGET_H2, SEC_TIMER_TARGET_H2, -1);
+                interval_flag = true;
+              }else if(!H1_null_flag && H2_null_flag){
+                timeMapMS(MIN_TIMER_TARGET_H1, SEC_TIMER_TARGET_H1, -1);
+                interval_flag = true;
+              }else{
+                 timeMapMS(MIN_TIMER_TARGET_H1, SEC_TIMER_TARGET_H1, -1);
+              }
+              
               resetTimerTamps();
-              rounds_target_display_flag = (RD_TIMER_TARGET == 1 && ((MIN_TIMER_TARGET_H1 + SEC_TIMER_TARGET_H1) == 0 || (MIN_TIMER_TARGET_H2 + SEC_TIMER_TARGET_H2) == 0))? true : false;
+              rounds_target_display_flag = (RD_TIMER_TARGET == 1 && ( H1_null_flag || H2_null_flag ))? true : false;
               previousMillis = millis();
               buzzerOn(1000);
           }
@@ -128,7 +143,7 @@ void intervalTimerUP(){
                 buzzerOn(1000); //buzz each round
                 rounds_flag = false;
               }
-              //Serial.println("--");
+              
               if( (MIN_TIMER == tabTimeMapMS[0] && SEC_TIMER > tabTimeMapMS[1]) || (SEC_TIMER == 0 && tabTimeMapMS[1] == 59 && MIN_TIMER == tabTimeMapMS[0] + 1 )){
                 MIN_TIMER = 0;
                 SEC_TIMER = 0;
@@ -136,8 +151,10 @@ void intervalTimerUP(){
                 if(interval_flag){
                   rounds_flag = true;
                 }
-                interval_flag = !interval_flag;
-                interval_flag ? timeMapMS(MIN_TIMER_TARGET_H2, SEC_TIMER_TARGET_H2, -1) : timeMapMS(MIN_TIMER_TARGET_H1, SEC_TIMER_TARGET_H1, -1);
+                if(!H1_null_flag && !H2_null_flag){
+                  interval_flag = !interval_flag;
+                  interval_flag ? timeMapMS(MIN_TIMER_TARGET_H2, SEC_TIMER_TARGET_H2, -1) : timeMapMS(MIN_TIMER_TARGET_H1, SEC_TIMER_TARGET_H1, -1);
+                }
                 buzzerOn(800); //buzz each intervall
               }
 
@@ -146,6 +163,7 @@ void intervalTimerUP(){
               }else{
                 dots(1, true);
               }
+              
           }else if(!pause_flag){
             //To finish on what it has been set
             if (MIN_TIMER_TARGET_H2+SEC_TIMER_TARGET_H2==0){
